b613408337cf3ee20f2d558786d1f05a4ae83e3e
heron88 <heron88@web.de>
2016-01-27 12:39:38 +0100

    d

--- diagramm.js
+++ diagramm.js
@@ -1,5 +1,7 @@
 $(document).ready(function(){
     $("button").click(function(){
-        $(".balken1").fadeOut("slow")
+        $(".balken1").fadeOut("slow") ;
     })
-})
+})     ;
+$(document).ready(function(){
+    $("div").hide()});
